6a1cf13fcc4abe1ce24179ed4567ef00957ca4cd
resaUndefined <resamuhammad96@gmail.com>
2017-08-30 09:47:05 +0700

    push pertama

---
+++ web_sekolah/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from web_sekolah.models import Agama,Siswa
+
+# Register your models here.
+admin.site.register(Agama)
+admin.site.register(Siswa)
+
